b5b06d02928660a68dd430dab8155a13af658a48
Wellson Almeida dos Santos <wellson.digital@gmail.com>
2026-01-19 13:17:57 -0300

    feat: Implement dengue diagnosis ML model pipeline in Jupyter notebook and add a run script.

---
+++ rodar.sh
@@ -0,0 +1,22 @@
+
+cd /Users/wellsonalmeida/workspace/pessoal/fiap/pos/Tech-Challenge-Fase-1
+pip install -r requirements.txt
+jupyter notebook tech_challenge_fase1_dengue2022.ipynb
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
